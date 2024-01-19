2eb3d2fdefe5d1044c10b2445eb094cb9a52a8c9
tarek <taga61188@gmail.com>
2024-01-19 07:25:41 -0800

    CM

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
-# install flask from pip3
-
-package { 'Flask':
-  ensure	=> '2.1.0',
-  provider	=> 'pip3',
+#!/usr/bin/pup
+# Install a specific version of flask (2.1.0)
+package {'flask':
+  nsure => '2.1.0',
+  provider => 'pip',
 }
