4867250196a09b03c108977f68698b956477a8e9
tarek <taga61188@gmail.com>
2024-01-19 21:38:02 -0800

    up date

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,18 @@
 #!/usr/bin/pup
 # install a specific version of flask (2.1.0)
 
-package {'flask':
+package { 'python3-pip':
+  ensure => installed,
+}
+
+package { 'Flask':
   ensure   => '2.1.0',
-  provider => 'pip'
+  provider => 'pip3',
+  require  => Package['python3-pip'],
+}
+
+package { 'werkzeug':
+  ensure   => '2.0.1',
+  provider => 'pip',
+  require  => Package['python3-pip'],
 }
